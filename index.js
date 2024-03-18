ff253e69fe7c447a03f7812080858123475acb33
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-18 21:18:15 +0300

    add currentUser

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Conor Maynard';
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!'; 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+typeof "const currentUser = 'Conor Maynard';";
+"const currentUser = 'Conor Maynard';".length;
+const shortGreeting = 'Welcome, '+ 'Conor Maynard'[0] + '!';
+
